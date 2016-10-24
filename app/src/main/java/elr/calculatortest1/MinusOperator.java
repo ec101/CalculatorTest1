2e91108e99c523b7de0101d03e77f56192ddf28f
ec101 <emmetclarke@gmail.com>
2016-10-24 22:19:46 +0100

    Adding MinusOperator

---
+++ app/src/main/java/elr/calculatortest1/MinusOperator.java
@@ -0,0 +1,10 @@
+package elr.calculatortest1;
+
+class MinusOperator extends AbstractBinaryOperator {
+
+    @Override
+    public Double calculate(Double x, Double y) {
+        return x-y;
+    }
+
+}
---
+++ app/src/test/java/elr/calculatortest1/MinusOperatorTest.java
@@ -0,0 +1,50 @@
+package elr.calculatortest1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MinusOperatorTest {
+
+    @Test
+    public void testThreeMinusTwo() throws Exception {
+        MinusOperator minusOperator = new MinusOperator();
+        Double result = minusOperator.calculate(3.0, 2.0);
+        assertTrue("Minus NOT working correctly!", result==1.0);
+    }
+
+    @Test
+    public void testThreeMinusZeroCase() throws Exception {
+        MinusOperator minusOperator = new MinusOperator();
+        Double result = minusOperator.calculate(3.0, 0.0);
+        assertTrue("Minus NOT working correctly!", result==3.0);
+    }
+
+    @Test
+    public void testZeroMinusZeroCase() throws Exception {
+        MinusOperator minusOperator = new MinusOperator();
+        Double result = minusOperator.calculate(0.0, 0.0);
+        assertTrue("Minus NOT working correctly!", result==0.0);
+    }
+
+    @Test
+    public void testTenMinusOneCase() throws Exception {
+        MinusOperator minusOperator = new MinusOperator();
+        Double result = minusOperator.calculate(10.0, 1.0);
+        assertTrue("Minus NOT working correctly!", result==9);
+    }
+
+    @Test
+    public void testZeroMinusOneCase() throws Exception {
+        MinusOperator minusOperator = new MinusOperator();
+        Double result = minusOperator.calculate(0.0, 1.0);
+        assertTrue("Minus NOT working correctly!", result==-1.0);
+    }
+
+    @Test
+    public void testZeroMinusNegativeOneCase() throws Exception {
+        MinusOperator minusOperator = new MinusOperator();
+        Double result = minusOperator.calculate(0.0, -1.0);
+        assertTrue("Minus NOT working correctly!", result==1);
+    }
+}
