8a80e1d311259a0ffec3a57e89c0a81f0e1b3910
ec101 <emmetclarke@gmail.com>
2016-11-02 18:48:25 +0000

    Adding MinusOperator

--- app/src/main/java/elr/calculatortest1/MainActivity.java
+++ app/src/main/java/elr/calculatortest1/MainActivity.java
@@ -15,41 +15,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initSpinner();
+        //initSpinner();
         final Calculator calculator = new Calculator();
 
-        final Button calculateButton = (Button)findViewById(R.id.button);
-        View.OnClickListener listener = new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
 
-                EditText xValueTextField = (EditText)findViewById(R.id.xValue);
-                Integer xValue = getNumericValue(xValueTextField);
-
-                EditText yValueTextField = (EditText)findViewById(R.id.yValue);
-                Integer yValue = getNumericValue(yValueTextField);
-
-                Spinner spinner = (Spinner)findViewById(R.id.spinner);
-                Operator selectedOperator = Operator.getOperator((String)spinner.getSelectedItem());
-
-                Integer result = calculator.calculate(xValue, selectedOperator, yValue);
-
-                EditText resultTextField = (EditText)findViewById(R.id.resultValue);
-                resultTextField.setText(result.toString());
-            }
-        };
-        calculateButton.setOnClickListener(listener);
+//        final Button calculateButton = (Button)findViewById(R.id.plus_button);
+//        View.OnClickListener listener = new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//
+//                EditText xValueTextField = (EditText)findViewById(R.id.xValue);
+//                Integer xValue = getNumericValue(xValueTextField);
+//
+//                EditText yValueTextField = (EditText)findViewById(R.id.yValue);
+//                Integer yValue = getNumericValue(yValueTextField);
+//
+//                Spinner spinner = (Spinner)findViewById(R.id.spinner);
+//                Operator selectedOperator = Operator.getOperator((String)spinner.getSelectedItem());
+//
+//                Integer result = calculator.calculate(xValue, selectedOperator, yValue);
+//
+//                EditText resultTextField = (EditText)findViewById(R.id.resultValue);
+//                resultTextField.setText(result.toString());
+//            }
+//        };
+//        calculateButton.setOnClickListener(listener);
     }
 
     private Integer getNumericValue(EditText textField) {
         return Integer.valueOf(textField.getText().toString());
     }
 
-    private void initSpinner(){
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.operators, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-    }
+//    private void initSpinner(){
+//        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//                R.array.operators, android.R.layout.simple_spinner_item);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(adapter);
+//    }
 }
